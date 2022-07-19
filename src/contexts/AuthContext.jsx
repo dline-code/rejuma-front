71c9f22a1ad8b08a03971d3ef524da957b994e51
José Gonçalves <goncalves2541@gmail.com>
2022-07-19 00:19:10 +0000

    define history

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -3,12 +3,17 @@ import Swal from 'sweetalert2'
 
 import api from 'src/services/api'
 
+import { useHistory } from 'react-router-dom'
+
+
 const AuthContext = createContext({})
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [authenticated, setAuthenticated] = useState(false)
   const [loading, setLoading] = useState(true)
+  const history = useHistory()
+
 
   async function recoverUserInfo() {
     const USER_ID = JSON.parse(String(localStorage.getItem('user-id')))
