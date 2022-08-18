34c76af0801f08dd666e9c88a3101e8dbde5e71e
mariovarela99 <mariovarela99@outlook.com>
2022-08-18 23:22:38 +0100

    feat: Add New routes

--- src/routes.js
+++ src/routes.js
@@ -4,25 +4,8 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Matriculas = React.lazy(() => import('./views/matriculas'))
 const NewMatricula = React.lazy(() => import('./views/matriculas/new'))
 const Propinas = React.lazy(() => import('./views/propinas'))
-
-//Forms
-const ChecksRadios = React.lazy(() => import('./views/forms/checks-radios/ChecksRadios'))
-const FloatingLabels = React.lazy(() => import('./views/forms/floating-labels/FloatingLabels'))
-const FormControl = React.lazy(() => import('./views/forms/form-control/FormControl'))
-const InputGroup = React.lazy(() => import('./views/forms/input-group/InputGroup'))
-const Layout = React.lazy(() => import('./views/forms/layout/Layout'))
-const Range = React.lazy(() => import('./views/forms/range/Range'))
-const Select = React.lazy(() => import('./views/forms/select/Select'))
-const Validation = React.lazy(() => import('./views/forms/validation/Validation'))
-
-const Charts = React.lazy(() => import('./views/charts/Charts'))
-
-// Icons
-const CoreUIIcons = React.lazy(() => import('./views/icons/coreui-icons/CoreUIIcons'))
-const Flags = React.lazy(() => import('./views/icons/flags/Flags'))
-const Brands = React.lazy(() => import('./views/icons/brands/Brands'))
-
-const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
+const PropinaDetails = React.lazy(() => import('./views/propinas/details'))
+const NewPropinaPayment = React.lazy(() => import('./views/propinas/new'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
@@ -30,21 +13,12 @@ const routes = [
   { path: '/matriculas', name: 'Matrículas', component: Matriculas, exact: true },
   { path: '/matriculas/create', name: 'Criar Matricula', component: NewMatricula },
   { path: '/propinas', name: 'Propinas', component: Propinas },
-  { path: '/charts', name: 'Charts', component: Charts },
-  { path: '/forms', name: 'Forms', component: FormControl, exact: true },
-  { path: '/forms/form-control', name: 'Form Control', component: FormControl },
-  { path: '/forms/select', name: 'Select', component: Select },
-  { path: '/forms/checks-radios', name: 'Checks & Radios', component: ChecksRadios },
-  { path: '/forms/range', name: 'Range', component: Range },
-  { path: '/forms/input-group', name: 'Input Group', component: InputGroup },
-  { path: '/forms/floating-labels', name: 'Floating Labels', component: FloatingLabels },
-  { path: '/forms/layout', name: 'Layout', component: Layout },
-  { path: '/forms/validation', name: 'Validation', component: Validation },
-  { path: '/icons', exact: true, name: 'Icons', component: CoreUIIcons },
-  { path: '/icons/coreui-icons', name: 'CoreUI Icons', component: CoreUIIcons },
-  { path: '/icons/flags', name: 'Flags', component: Flags },
-  { path: '/icons/brands', name: 'Brands', component: Brands },
-  { path: '/widgets', name: 'Widgets', component: Widgets },
+  { path: '/propinas/:id', name: 'Propinas - Detalhes', component: PropinaDetails },
+  {
+    path: '/propinas/create',
+    name: 'Adicionar Novo Pagamento de Propinas',
+    component: NewPropinaPayment,
+  },
 ]
 
 export default routes
