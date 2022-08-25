acf1581424d3da7ef2bebd733fa0b945ae6ba236
Goreth Manuel <gorethmanuel0@gmail.com>
2022-08-25 18:39:24 +0100

    Se aceita adiciona novas alteracoes

--- src/views/cadastro/new/index copy.js
+++ src/views/cadastro/new/index copy.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Stepper, Step } from 'react-form-stepper'
-import StepWizard from 'react-step-wizard'
+import { Stepper, Step } from 'react-form-stepper';
+import StepWizard from 'react-step-wizard';
 import {
   CCard,
   CCardBody,
@@ -14,7 +14,7 @@ import {
   CRow,
 } from '@coreui/react'
 
-function NewMatricula() {
+function NewCadastro() {
   const [trabalha, setTrabalha] = useState(false)
   return (
     <>
@@ -136,4 +136,4 @@ function NewMatricula() {
   )
 }
 
-export default NewMatricula
+export default NewCadastro
---
+++ src/views/cadastro/new/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Stepper, Step } from 'react-form-stepper'
+import StepWizard from 'react-step-wizard'
+import { CCard, CCardBody, CCardHeader, CContainer } from '@coreui/react'
+import { MdDescription } from 'react-icons/md'
+
+import Step1 from './step1'
+import Step2 from './step2'
+import Step3 from './step3'
+
+function NewCadastro() {
+  const [stepWizard, setStepWizard] = useState(null)
+  const [user, setUser] = useState({})
+  const [activeStep, setActiveStep] = useState(0)
+
+  const assignStepWizard = (instance) => {
+    setStepWizard(instance)
+  }
+
+  const handleStepChange = (e) => {
+    setActiveStep(e.activeStep - 1)
+  }
+
+  const handleComplete = () => {
+    console.log('finnaly')
+  }
+
+  const assignUser = (val) => {
+    console.log(val)
+    setUser((user) => ({
+      ...user,
+      ...val,
+    }))
+  }
+
+  return (
+    <>
+      <CContainer>
+        <CCard>
+          <CCardHeader>Criar Nova Matrícula</CCardHeader>
+          <CCardBody>
+            <Stepper activeStep={activeStep}>
+              <Step label="Entidade">
+                <MdDescription />
+              </Step>
+              <Step label="Detalhes da Conta" />
+              <Step label="Confirmar" />
+            </Stepper>
+            <StepWizard instance={assignStepWizard} onStepChange={handleStepChange}>
+              <Step1 userCallback={assignUser} />
+              <Step2 user={user} userCallback={assignUser} />
+              <Step3 user={user} completeCallback={handleComplete} />
+            </StepWizard>
+          </CCardBody>
+        </CCard>
+      </CContainer>
+    </>
+  )
+}
+
+export default NewCadastro
