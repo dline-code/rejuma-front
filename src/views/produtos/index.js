a35dd7dfbe5c2d2d061e19086ded43b3e0d8c6fc
Goreth Manuel <gorethmanuel0@gmail.com>
2022-09-02 15:22:32 +0100

    Se aceita adiciona atualizacoes

--- src/views/produtos/index.js
+++ src/views/produtos/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable prettier/prettier */
 import { 
-    //CButton,
+    CButton,
     CCard,
     CCardBody,
     CCardHeader,
@@ -14,110 +14,145 @@ import {
     CTableHead,
     CTableHeaderCell,
     CTableRow,
+    CModal,
+    CModalHeader,
+    CModalTitle,
+    CModalBody,
+    CModalFooter
   
   } from '@coreui/react'
-  import React from 'react'
+  import React, { useState } from 'react'
   import CIcon from '@coreui/icons-react'
-  import { cilDelete } from '@coreui/icons'
-  import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+  import { cilDelete, cilTrash } from '@coreui/icons'
+  //import { Link } from 'react-router-dom/cjs/react-router-dom.min'
   
   
   function Produtos () {
+    const [ visible , setVisible ] = useState ( false )  
+
       return (
           <>
           <CContainer>
               <CCard>
-                  <CCardHeader
-                      style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                <CCardHeader
+                    style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
                   >
-                      <CCardTitle> Produtos </CCardTitle>
-                      <Link to="/produtos/create">
-                    <CButton>Adicionar Produtos</CButton>
-                    </Link>
-                  </CCardHeader>
+                    <CCardTitle> Produtos </CCardTitle>
+                    <CButton onClick = { ( ) => setVisible ( ! visible ) }  >Adicionar Produto</CButton> 
+                </CCardHeader>
   
                   <CCardBody>
                       <CTable bordered>
                           <CTableHead>
                               <CTableRow>
-                                  <CTableHeaderCell>#</CTableHeaderCell>
+                                  <CTableHeaderCell style={{ textAlign: 'center' }} >#</CTableHeaderCell>
                                   <CTableHeaderCell width={'35%'}>Nome</CTableHeaderCell>
-                                  <CTableHeaderCell width={'25%'}>Venda</CTableHeaderCell>
-                                  <CTableHeaderCell>Stock</CTableHeaderCell>
-                                  <CTableHeaderCell>Editar</CTableHeaderCell>
-                                  <CTableHeaderCell>Excluir</CTableHeaderCell>
+                                  <CTableHeaderCell width={'25%'} style={{ textAlign: 'center' }} >Venda</CTableHeaderCell>
+                                  <CTableHeaderCell style={{ textAlign: 'center' }} >Stock</CTableHeaderCell>
+                                  <CTableHeaderCell style={{ textAlign: 'center' }} >Editar</CTableHeaderCell>
+                                  <CTableHeaderCell style={{ textAlign: 'center' }} >Excluir</CTableHeaderCell>
                               </CTableRow>
                           </CTableHead>
   
                           <CTableBody>
   
                           <CTableRow>
-                          <CTableDataCell> 1 </CTableDataCell>
-                          <CTableDataCell> Goreth Manuel </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 1 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           
-                          <CTableDataCell> <CIcon icon={cilDelete}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 2 </CTableDataCell>
-                          <CTableDataCell> Albertina Jorge </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 2 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 4 </CTableDataCell>
-                          <CTableDataCell> Mário Varela </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 4 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 5 </CTableDataCell>
-                          <CTableDataCell> Suzana Pereira </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 5 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 6 </CTableDataCell>
-                          <CTableDataCell> Luzia José </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 6 </CTableDataCell>
+                          <CTableDataCell > Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
+                          
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 7 </CTableDataCell>
-                          <CTableDataCell> Osvaldo de Sousa </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 7 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                          <CTableDataCell> 8 </CTableDataCell>
-                          <CTableDataCell> Magarida André </CTableDataCell>
-                          <CTableDataCell> 009696435LA190 </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 8 </CTableDataCell>
+                          <CTableDataCell> Uniforme </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                           <CTableDataCell> M </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                          <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
   
                           <CTableRow>
-                              <CTableDataCell> 9 </CTableDataCell>
-                              <CTableDataCell> Paulino Passil </CTableDataCell>
-                              <CTableDataCell> 009696435LA190 </CTableDataCell>
+                              <CTableDataCell style={{ textAlign: 'center' }} > 9 </CTableDataCell>
+                              <CTableDataCell> Uniforme </CTableDataCell>
+                              <CTableDataCell style={{ textAlign: 'center' }} > 009696435LA190 </CTableDataCell>
                               <CTableDataCell> M </CTableDataCell>
-                             
+                              <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilDelete}/> </CTableDataCell>
+                              <CTableDataCell style={{ textAlign: 'center' }} > <CIcon icon={cilTrash}/> </CTableDataCell>
                           </CTableRow>
                           </CTableBody>
   
                           <CTableFoot></CTableFoot>
                       </CTable>
                    </CCardBody>
-                                  </CCard>
-                              </CContainer>
-          
-       </>
+                </CCard>
+            </CContainer>
+
+        < CModal visible = { visible } onClose = { ( ) => setVisible ( false ) } >    
+            < CModalHeader onClose = { ( ) => setVisible ( false ) } >   
+                <CModalTitle> Título modal </CModalTitle>
+            </ CModalHeader >
+
+            < CModalBody > Woohoo, você está lendo este texto em um modal! </ CModalBody >
+
+            < CModalFooter >
+                < CButton color = " secundário " onClick = { ( ) => setVisible ( false ) } >    
+                Perto
+                </CButton >
+                < CButton color = " primary " > Salvar alterações </ CButton > 
+            </ CModalFooter >
+        </CModal >
+  </ >
+       
     )
   }
   
