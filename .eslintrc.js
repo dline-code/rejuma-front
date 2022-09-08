d59ed5e0429e43e4bce27777db5e760e7a025886
Goreth Manuel <gorethmanuel0@gmail.com>
2022-09-08 17:33:03 +0100

    Se aceita adiciona atualizacoes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,11 @@ module.exports = {
   },
   extends: [
     'react-app',
+    'prettier',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
